ada850bfdbc234f00bb0c9ac4a97b3883d75bfa6
sankara prasanth <prasanth@inflolabs.com>
2019-10-07 06:23:18 +0530

    vertex hazlecast sample

---
+++ src/main/java/demo/Launcher.java
@@ -0,0 +1,29 @@
+package demo;
+
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.config.Config;
+import com.hazelcast.core.HazelcastInstance;
+
+import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
+import io.vertx.core.eventbus.EventBus;
+import io.vertx.core.spi.cluster.ClusterManager;
+import io.vertx.spi.cluster.hazelcast.HazelcastClusterManager;
+
+public class Launcher {
+	public static EventBus clusterEB = null;
+
+	public static void main(String[] args) {
+		ClusterManager mgr = new HazelcastClusterManager();
+
+		VertxOptions options = new VertxOptions().setClusterManager(mgr).setClusterHost("195.201.90.61");
+		Vertx.clusteredVertx(options, res -> {
+			if (res.succeeded()) {
+				Vertx vertx = res.result();
+				clusterEB = vertx.eventBus();
+				vertx.deployVerticle(new DemoVerticle());
+			}
+		});
+	}
+}
---
+++ src/main/java/demo/LauncherClient.java
@@ -0,0 +1,34 @@
+package demo;
+
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.core.HazelcastInstance;
+
+import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
+import io.vertx.core.eventbus.EventBus;
+import io.vertx.core.spi.cluster.ClusterManager;
+import io.vertx.spi.cluster.hazelcast.HazelcastClusterManager;
+
+public class LauncherClient {
+	public static EventBus clusterEB = null;
+	public static void main(String args[]) {
+		ClientConfig config = new ClientConfig();
+		String[] addresses = { "195.201.90.61" + ":" + "5701" };
+		config.getNetworkConfig().setSmartRouting(true).addAddress(addresses);
+
+		// start Hazelcast client
+		HazelcastInstance hazelcastInstance = HazelcastClient.newHazelcastClient(config);
+		ClusterManager mgr = new HazelcastClusterManager(hazelcastInstance);
+		VertxOptions options = new VertxOptions().setClusterManager(mgr);
+		Vertx.clusteredVertx(options, res -> {
+			if (res.succeeded()) {
+				Vertx vertx = res.result();
+				clusterEB = vertx.eventBus();
+				System.out.println("connected to  hazlecast cluster");
+			} else {
+				System.out.println("failed");
+			}
+		});
+	}
+}
